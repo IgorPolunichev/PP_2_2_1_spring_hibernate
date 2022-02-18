60f703f4b89eeda141a42cd81cd8b7bb686036f8
IgorPolunichev <93341324+IgorPolunichev@users.noreply.github.com>
2022-02-18 23:07:33 +0300

    2.2.1 CHECK

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,10 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("ВАЗ", 2101)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("GAZ", 3110)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("UAZ", 3303)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Lada", 2111)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,9 +27,17 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Model and serial Car = "
+                 + user.getUserCar().getModel()
+                 + " "
+                 + user.getUserCar().getSeries());
          System.out.println();
       }
-
+      User user = userService.getUser("ВАЗ" , 2101);
+      System.out.println("Id = "+user.getId());
+      System.out.println("First Name = "+user.getFirstName());
+      System.out.println("Last Name = "+user.getLastName());
+      System.out.println("Email = "+user.getEmail());
       context.close();
    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -26,4 +26,10 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Transactional
+   @Override
+   public User getUser(String model, Integer serial){
+      return userDao.getUser(model, serial);
+   }
+
 }
